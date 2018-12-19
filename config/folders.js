f73197ea63d200e005612fa8291c97663cd6c8c1
Douglas Panhota <douglaspands@gmail.com>
2018-12-19 12:35:29 -0200

    Mudança no nome do arquivo que representa o route.js

--- config/folders.js
+++ config/folders.js
@@ -3,9 +3,11 @@ const path = require('path');
 const ROOT = path.join(__dirname, '../');
 const LIB = path.join(__dirname, '../lib');
 const APP = path.join(__dirname, '../app');
+const FIRST_FILE = 'index.js';
 
 module.exports = {
     ROOT,
     LIB,
-    APP
+    APP,
+    FIRST_FILE
 }
--- config/index.js
+++ config/index.js
@@ -1,9 +1,11 @@
 const server = require('./server');
 const logger = require('./logger');
 const folder = require('./folders');
+const constants = require('./constants');
 
 module.exports = {
     server,
     logger,
-    folder
+    folder,
+    constants
 }
